2bc40453c6567bec69c92e66faefba6f2268aecb
ad1tyas1ngh <adityasingh1768@gmail.com>
2021-11-10 18:42:11 +0530

    sprd-common: treewide cleanup

--- common.mk
+++ common.mk
@@ -18,67 +18,54 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Audio
 PRODUCT_PACKAGES += \
-	audio.a2dp.default \
-	audio.usb.default \
-	audio.r_submix.default \
-	libtinyalsa
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    libtinyalsa
 
 # WiFi
 PRODUCT_PACKAGES += \
-	dhcpcd.conf \
-	wpa_supplicant \
-	wpa_supplicant.conf \
-	hostapd
+    dhcpcd.conf \
+    wpa_supplicant \
+    hostapd
 
 # Device props
 PRODUCT_PROPERTY_OVERRIDES := \
-	keyguard.no_require_sim=true \
-	ro.com.android.dataroaming=false
-
-# Compat
-PRODUCT_PACKAGES += \
-    	libstlport
-
-# Set default USB interface
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=mtp
+    keyguard.no_require_sim=true
 
 # Charger
 PRODUCT_PACKAGES += \
-	charger \
-	charger_res_images
+    charger \
+    charger_res_images
 
 # Permissions
-PERMISSION_XML_FILES := \
-	frameworks/native/data/etc/handheld_core_hardware.xml \
-	frameworks/native/data/etc/android.hardware.bluetooth_le.xml \
-	frameworks/native/data/etc/android.hardware.telephony.gsm.xml \
-	frameworks/native/data/etc/android.hardware.location.xml \
-	frameworks/native/data/etc/android.hardware.location.gps.xml \
-	frameworks/native/data/etc/android.hardware.wifi.xml \
-	frameworks/native/data/etc/android.hardware.wifi.direct.xml \
-	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml \
-	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml \
-	frameworks/native/data/etc/android.hardware.touchscreen.xml \
-	frameworks/native/data/etc/android.software.sip.xml \
-	frameworks/native/data/etc/android.software.sip.voip.xml \
-	frameworks/native/data/etc/android.hardware.usb.accessory.xml \
-
 PRODUCT_COPY_FILES += \
-	$(foreach f,$(PERMISSION_XML_FILES),$(f):system/etc/permissions/$(notdir $(f)))
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/native/data/etc/android.software.sip.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.sip.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.usb.accessory.xml
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-	f2fstat \
-	fibmap.f2fs \
-	fsck.f2fs \
-	mkfs.f2fs \
-	setup_fs \
+    f2fstat \
+    fibmap.f2fs \
+    fsck.f2fs \
+    mkfs.f2fs \
+    setup_fs \
 
 # Misc packages
 PRODUCT_PACKAGES += \
-	com.android.future.usb.accessory \
-    
+    com.android.future.usb.accessory
+
 # Samsung Service Mode
 PRODUCT_PACKAGES += \
-	SamsungServiceMode
+    SamsungServiceMode
